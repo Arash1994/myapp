3e622bcaaf632a6e6c1aa036f6de00b51f8e3999
Arash <arshdeep@clickapps.co>
2017-10-23 11:32:54 +0530

    modify

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 20171018100604) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.bigint "category_id"
     t.index ["category_id"], name: "index_posts_on_category_id"
   end
